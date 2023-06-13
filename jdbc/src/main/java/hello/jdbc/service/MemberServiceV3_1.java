acb4b8f00a9dd7bc4f344b2ed268b759c12c5e56
Son won cheol <xzxz7003@naver.com>
2023-06-13 16:36:47 +0900

    <Spring-db12> :  스프링환경에서의 트랜잭션 처리 코드

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,70 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션, - 트랜잭션 매니저!
+ * */
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceV3_1 {
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 트랜잭션! START!
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+         
+            // 비즈니스로직
+            bizLogic( fromId, toId, money);
+            // 커밋 , OR 롤백
+          transactionManager.commit(status); // 성공시 커밋!
+        }catch (Exception e){
+            transactionManager.rollback(status); // 실패!
+            throw new IllegalStateException(e);
+        }  // 트랜잭션 매니저가 release 관련 다 처리해준다.
+
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById( fromId);
+        Member toMember = memberRepository.findById( toId);
+
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        validation(fromMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")){
+            throw new IllegalStateException("이체 중 에외 발생");
+        }
+    }
+
+    private static void release(Connection con) {
+        if(con !=null){
+            try {
+                con.setAutoCommit(true); // 커넥션 풀 고려
+                con.close();
+            }catch (Exception e){
+                log.info("error",e);
+            }
+
+        }
+    }
+}
