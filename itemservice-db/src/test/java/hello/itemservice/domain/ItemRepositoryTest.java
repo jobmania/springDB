d9c551c183284e25ecf5bb5ca3043a1f419a3f28
jobmania <xzxz7003@naver.com>
2023-06-23 03:57:30 +0900

    <Spring-db 2> : Test 에서 commit 사용시

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
@@ -49,6 +51,8 @@ class ItemRepositoryTest {
 
 
 
+//    @Commit
+//    @Transactional
     @Test
     void save() {
         //given
