803d035d00d9cf8d020e2ade91ac061d07fc3b29
jobmania <xzxz7003@naver.com>
2023-06-26 00:35:07 +0900

    <Spring-db 2> : Mybatis 설정  및 테스트
    - config
    - Mapper 등록
    - xml 작성

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -0,0 +1,23 @@
+package hello.itemservice.repository.mybatis;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface ItemMapper {
+
+    void save(Item item);
+
+    void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto updateParam); // 파라미터 2개일땐 선언
+
+    Optional<Item> findById(Long id);
+
+    List<Item> findAll(ItemSearchCond itemSearchCond);
+
+}
