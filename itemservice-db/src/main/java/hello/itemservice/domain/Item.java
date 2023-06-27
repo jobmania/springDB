a4845ab6cae6b109d03de6811d7b3761a827d36e
jobmania <xzxz7003@naver.com>
2023-06-28 05:29:34 +0900

    <Spring-db 2 > : Jpa 등록

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -1,12 +1,19 @@
 package hello.itemservice.domain;
 
 import lombok.Data;
+import lombok.Generated;
+
+import javax.persistence.*;
 
 @Data
+@Entity
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //  db에서 값이 증가~
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
