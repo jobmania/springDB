abae4d947f306b6a71fcd74dc3cd33279c283f04
jobmania <xzxz7003@naver.com>
2023-07-04 03:43:23 +0900

    <Spring-db 2 > : 클래스 내부 메소드 호출시 트랜잭션이 적용되지 않는 예시

--- springtx/src/test/java/hello/springtx/apply/InternalCallV1Test.java
+++ springtx/src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -1,2 +1,67 @@
-package hello.springtx.apply;public class InternalCallV1Test {
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@SpringBootTest
+@Slf4j
+public class InternalCallV1Test {
+
+
+    @Autowired
+    CallService callService;
+
+
+    @Test
+    void printProxy(){
+        log.info("callService class={}",callService.getClass());
+    }
+
+    @Test
+    void internalCall(){
+        callService.internal();
+    }
+
+
+    @Test
+    void externalCall(){
+        callService.external();
+    }
+
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig{
+        @Bean
+        CallService callService(){
+            return new CallService();
+        }
+    }
+
+
+    static class CallService{
+        public void external(){
+            log.info("call external");
+            printTxInfo();
+            internal(); // this. 나 자신의 인스턴스 주소!!
+        }
+
+        @Transactional
+        public void internal(){
+            log.info("call internal");
+            printTxInfo();
+        }
+
+
+        private void printTxInfo(){
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+
+        }
+    }
 }
