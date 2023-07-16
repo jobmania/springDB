efb235e5319d46ec3f0038c61e703985cb5e3939
jobmania <xzxz7003@naver.com>
2023-07-16 12:48:07 +0900

    <Spring-db 2 > : 로직마다 새로운 트랜잭션 생성
    1. 각각의 트랜잭션은 각각 물리트랜잭션으로 되어 커밋,롤백되더라도 나머지 트랜잭션 영향을 주지 않는다.

--- springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
@@ -150,4 +151,25 @@ public class BasicTxTest {
                 .isInstanceOf(UnexpectedRollbackException.class);
     }
 
+
+
+    @Test
+    void inner_rollback_requires_new(){
+        log.info("외부 트랜잭션 시작" );
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}",outer.isNewTransaction());
+
+
+        log.info("내부 트랜잭션ㅅ ㅣ작 ");
+        DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW); // default는 기존 트랜잭션에 참여
+        // new 는 기존의 트랜잭션을 무시하고 새 트랜잭션을 만듬
+        TransactionStatus inner = txManager.getTransaction(definition);
+        log.info("inner.isNewTransaction()={}",inner.isNewTransaction()); // true
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
 }
